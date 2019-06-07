89042fd7942d9a392beec5c1bf40138177f07cd1
trangtpham <51464846+trangtpham@users.noreply.github.com>
2019-06-07 15:33:08 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -71,11 +71,11 @@ $(window).on('load', function() {
 	/*------------------
 		Popup
 	--------------------*/
-	$('.portfolio-item .port-pic').magnificPopup({
-		type: 'image',
-		mainClass: 'img-popup-warp',
-		removalDelay: 500,
-	});
+// 	$('.portfolio-item .port-pic').magnificPopup({
+// 		type: 'image',
+// 		mainClass: 'img-popup-warp',
+// 		removalDelay: 500,
+// 	});
 
 
 
